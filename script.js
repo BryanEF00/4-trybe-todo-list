0e6eb2d6f37d62a4650e1035d8eeb9013c14b434
Bryan Fernandes <bryan.efernandes@gmail.com>
2021-12-21 17:46:32 -0300

    Requisitos 10 e 11

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ function tarefaCompleta(event) {
   let liSelected = event.target;
   liSelected.classList.toggle('completed');
 }
+
+/* Botão Apaga Tarefas */
+
+let btnApagaTudo = document.querySelector('#apaga-tudo');
+
+btnApagaTudo.addEventListener('click', apagaLista);
+
+function apagaLista() {
+  let itensLista = listaTarefa.children;
+  let listaSize = itensLista.length;
+  for (let i = 0; i < listaSize; i += 1) {
+    document.getElementsByTagName('li')[0].remove();
+  }
+}
+
+/* Botão Apaga Tarefas */
+
+let btnApagaCompletos = document.querySelector('#remover-finalizados');
+
+btnApagaCompletos.addEventListener('click', apagaCompletos);
+
+function apagaCompletos() {
+  let itensCompletos = document.getElementsByClassName('completed');
+  let completosSize = itensCompletos.length;
+  for (let i = 0; i < completosSize; i += 1) {
+    itensCompletos[0].remove();
+  }
+}

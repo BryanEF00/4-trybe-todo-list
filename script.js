f481b7fe119ec3fa346bf4db84dc9cf525985d05
Bryan Fernandes <bryan.efernandes@gmail.com>
2021-12-21 17:13:33 -0300

    Refiz quase todo o código do zero e adicionei o Requisito 9

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
+/* Criar li */
+
+let tarefa = document.querySelector('#texto-tarefa');
 let btnCriaTarefa = document.querySelector('#criar-tarefa');
-let lista = document.querySelector('#lista-tarefas');
+let listaTarefa = document.querySelector('#lista-tarefas');
 
 function criaTarefa() {
-  let itemLista = document.createElement('li');
-  let inputText = document.querySelector('#texto-tarefa').value;
+  let li = document.createElement('li');
+  li.innerText = tarefa.value;
+  listaTarefa.appendChild(li);
+  tarefa.value = '';
 
-  itemLista.innerText = inputText;
-  lista.appendChild(itemLista);
-
-  addBackgroundColor();
-
-  document.querySelector('#texto-tarefa').value = '';
+  li.addEventListener('click', mudarBackground);
+  li.addEventListener('dblclick', tarefaCompleta);
 }
 
 btnCriaTarefa.addEventListener('click', criaTarefa);
 
-function addBackgroundColor() {
-  for (let i = 0; i < lista.children.length; i += 1) {
-    lista.children[i].addEventListener('click', function () {
-      for (let i = 0; i < lista.children.length; i += 1) {
-        lista.children[i].style.backgroundColor = 'white';
-      }
-      this.style.backgroundColor = 'gray';
-    });
+/* Colorir Background */
+
+function mudarBackground(event) {
+  let liSelected = event.target;
+  if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected');
   }
+  liSelected.classList.add('selected');
+}
+
+/* Tarefa Completa */
+
+function tarefaCompleta(event) {
+  let liSelected = event.target;
+  liSelected.classList.toggle('completed');
 }
